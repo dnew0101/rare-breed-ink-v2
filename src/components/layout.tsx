0dba06d001eb50af5a5c4b5a1ff445266a117675
David Newman <david.newman.dev1@gmail.com>
2024-07-27 15:47:37 -0700

    Init: RBI v2 website.

---
+++ src/components/layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import '../styles/global.css'; 
+
+interface LayoutProps {
+    pageTitle: string;
+    children: React.ReactNode;
+}
+
+
+//react functional component
+const Layout = ({ pageTitle, children }: LayoutProps) => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+        <main>
+            <h1>{pageTitle}</h1>
+            {children}
+        </main>
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Rare Breed Ink</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
